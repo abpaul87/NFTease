1b35841e80b87aa625ac28c9f0411a200f521e18
Nuno Costa <nm.cdc@hotmail.com>
2021-11-30 14:59:30 +0000

    gem not working for IOS

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   #metamask
-  post '/auth/:provider/callback', to: 'sessions#create'
-  post '/auth/ethereum', to: 'sessions#new'
+  #post '/auth/:provider/callback', to: 'sessions#create'
+  #post '/auth/ethereum', to: 'sessions#new'
   root to: 'pages#home' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
