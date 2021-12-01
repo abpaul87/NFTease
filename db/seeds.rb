2b3f30bc60436ab5d81c7358273e79e0d7613b88
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-12-01 16:15:08 +0000

    did some stuff probably worng

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
+Nft.delete_all
+Gallery.delete_all
 User.delete_all
 puts "deleting..."
 
@@ -23,8 +25,54 @@ new_user = User.create(
     wallet: '0x96ff1d6b5e9ca15f9e61b7e2130599005144fb28'
   )
 new_user.save
-puts "Completing seeds..."
+puts "Completing users..."
 puts new_user
+
+# puts "starting gallery creation..."
+
+# gallery = Gallery.new(
+#   user: new_user,
+#   name: new_user.wallet,
+#   selectors: {sdff: "dafds", erew: "eiwore"}
+# )
+# gallery.save
+# puts "finished gallery creation..."
+# puts gallery
+
+# puts 'Starting openseas api call...'
+
+# url = URI('https://api.opensea.io/api/v1/assets?format=json&limit=20&offset=0&order_direction=desc&owner=0x96ff1d6b5e9ca15f9e61b7e2130599005144fb28')
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+# request = Net::HTTP::get(url)
+# response = JSON.parse(request)
+# response['assets'].each do |asset|
+#   # if asset['id'] == Nft.where(user: new_user).opensea_id
+#   #   nft = Nft.where(user: new_user).where(opensea_id: asset['id'])
+#   #   new_nft = nft.update(current_owner: asset.dig('owner', 'address'), token_metadata: asset)
+#   #   new_nft.save
+#   # else
+#     nft = Nft.new(
+#       token_name: asset['name'],
+#       token_description: asset['description'],
+#       collection_name: asset.dig('collection', 'name'),
+#       collection_description: asset.dig('collection', 'description'),
+#       artist_name: asset.dig('creator', 'user', 'username'),
+#       image_url: asset['image_original_url'],
+#       animation_url: asset['animation_original_url'],
+#       current_owner: asset.dig('owner', 'address'),
+#       token_metadata: asset,
+#       image_url_small: asset['image_preview_url'],
+#       opensea_link: asset['permalink'],
+#       user: new_user
+#     )
+#     nft.save
+#   # end
+# end
+
+puts 'Finished OpenSea API Call...'
+
+# puts nft
 
 # User.all.each do |user|
 #   2.times do
