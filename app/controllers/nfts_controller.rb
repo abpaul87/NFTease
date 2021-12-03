29abb74b5eda49a5ba4fb61ef42ef829ad30f602
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-12-03 12:37:26 +0000

    pushed more

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -11,10 +11,12 @@ class NftsController < ApplicationController
 
   def update
     @nft = Nft.find(params[:id])
+    @nftes = Nft.where(gallery_id: params[:gallery_id])
+    count = @nftes.length
     if @nft.gallery_id.nil?
-      @nft.update(gallery_id: params[:gallery_id])
+      @nft.update(gallery_id: params[:gallery_id], gallery_order: count)
     else
-      @nft.update(gallery_id: nil)
+      @nft.update(gallery_id: nil, gallery_order: nil)
     end
     redirect_to edit_gallery_path(params[:gallery_id])
   end
