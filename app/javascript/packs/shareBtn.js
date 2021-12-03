9d4b9a85839f6a6cafea599ad8fe588753408140
Flynn Lebourgeois <flynnn@me.com>
2021-12-03 12:10:34 +0000

    buttons

--- app/javascript/packs/shareBtn.js
+++ app/javascript/packs/shareBtn.js
@@ -2,18 +2,23 @@ import copy from 'copy-to-clipboard';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-export const copyingText = () =>{
+export const copyingText = () => {
   const copyBtn = document.querySelector("#shareButtonShow")
-  copyBtn.addEventListener('click', (event) =>{
+
+  if (!copyBtn) {
+    return;
+  }
+
+  copyBtn.addEventListener('click', (event) => {
     console.log(copyBtn.dataset.url);
 
     // Copy with options
-    copy(copyBtn.dataset.url , {
-        debug: true,
-        message: 'Copied',
-      });
-    tippy("#shareButtonShow", {
-      content: 'My tooltip!',
+    copy(copyBtn.dataset.url, {
+      debug: true,
+      message: 'Copied'
     });
+
+    const tip = tippy(copyBtn, { content: 'Copied!' });
+    tip.show();
   });
-  }
+}
