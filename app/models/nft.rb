12daf777a6249b607d1556dd42dc3c42e685f048
Nuno Costa <nm.cdc@hotmail.com>
2021-12-03 12:53:21 +0000

    order more or less done and working

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -1,4 +1,11 @@
 class Nft < ApplicationRecord
   belongs_to :user
   belongs_to :gallery, optional: true
+
+  def previous
+    Nft.where("gallery_id = ? AND id < ?", self.gallery.id, self.id).last
+  end
+  def next
+    Nft.where("gallery_id = ? AND id > ?", self.gallery.id, self.id).first
+  end
 end
