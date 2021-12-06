182ee1697921313517b51142da9892a0eccd4b8b
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-12-06 18:24:12 +0000

    did videos

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -76,7 +76,7 @@ class GalleriesController < ApplicationController
           collection_name: asset.dig('collection', 'name'),
           collection_description: asset.dig('collection', 'description'),
           artist_name: asset.dig('creator', 'user', 'username') || asset.dig('collection', 'name'),
-          image_url: asset['image_original_url'] || asset['image_url'] || "https://res.cloudinary.com/jansommer/image/upload/v1638530952/nftease/no-image.png",
+          image_url: asset['image_url'] || asset['image_original_url'] || "https://res.cloudinary.com/jansommer/image/upload/v1638530952/nftease/no-image.png",
           animation_url: asset['animation_original_url'],
           current_owner: asset.dig('owner', 'address'),
           token_metadata: asset,
