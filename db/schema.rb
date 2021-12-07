b27a8d4c49e82838f8cdd64ecde6620fff2f9b7c
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-12-07 13:13:56 +0000

    yay or nay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_093618) do
+ActiveRecord::Schema.define(version: 2021_12_07_125024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_12_07_093618) do
     t.bigint "gallery_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "forsale"
     t.index ["gallery_id"], name: "index_nfts_on_gallery_id"
     t.index ["user_id"], name: "index_nfts_on_user_id"
   end
