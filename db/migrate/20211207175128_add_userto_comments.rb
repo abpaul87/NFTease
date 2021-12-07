30a7d094b65a589054f0839d7e30295634330d17
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-12-07 18:59:30 +0000

    some

--- db/migrate/20211207175128_add_userto_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddUsertoComments < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :comments, :user, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_07_175441) do
-ActiveRecord::Schema.define(version: 2021_12_07_154709) do
-
+ActiveRecord::Schema.define(version: 2021_12_07_155241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +46,10 @@ ActiveRecord::Schema.define(version: 2021_12_07_154709) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.bigint "nft_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.index ["nft_id"], name: "index_comments_on_nft_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "galleries", force: :cascade do |t|
