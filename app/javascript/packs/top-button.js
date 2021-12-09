9e4d5feaecb46aa1ed8ef56d160b781e55788622
Flynn Lebourgeois <flynnn@me.com>
2021-12-09 11:39:13 +0000

    top button

--- app/javascript/packs/top-button.js
+++ app/javascript/packs/top-button.js
@@ -1,10 +1,27 @@
-// export const scrollToTop = () => {
-//   $(window).scroll(function () {
-//     if ($(this).scrollTop() > 50) {
-//       $('.scrolltop:hidden').stop(true, true).fadeIn();
-//     } else {
-//       $('.scrolltop').stop(true, true).fadeOut();
-//     }
-//   });
-//   $(function () { $(".scroll").click(function () { $("html,body").animate({ scrollTop: $(".thetop").offset().top }, "1000"); return false }) })
-// }
+export const scrollToTop = () => {
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+}
